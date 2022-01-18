3f36c24282cbf9aeb4987c04d5186230e495b123
alexmur474 <alexmur474@gmail.com>
2022-01-18 15:08:10 +0300

    1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const tmi = require("tmi.js");
-const mongoose = require('mongoose')
 
 const { declOfNum } = require('./my_modules/declOfNum');
 
